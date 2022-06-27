d72ef812a9eedb7d258fef18383e8c249faa50fe
TuanDoan14112003 <tuandoan14112003@gmail.com>
2022-06-27 12:28:01 +1000

    fix 204 code

--- authentication/views.py
+++ authentication/views.py
@@ -5,20 +5,21 @@ import requests
 # Create your views here.
 
 class UserActivationView(generics.GenericAPIView):
-
     def get (self, request, uid, token):
         protocol = 'https://' if request.is_secure() else 'http://'
         web_url = protocol + request.get_host()
         post_url = web_url + "/auth/users/activation/"
         post_data = {'uid': uid, 'token': token}
         response = requests.post(post_url, data = post_data)
-        content = response.json()
+        
         status = response.status_code
-        if  status == 200:
-            return Response(content)
+
+        if status == 204:
+            return Response({'detail':"Your account has been activated"},status=status)
         elif status == 400:
-            return Response({'detail':"Invalid activation link"},status=response.status_code)
+            return Response({'detail':"Invalid activation link"},status=status)
         elif status == 403:
-            return Response({'detail':"This activation link has already been used"},status=response.status_code)
+            return Response({'detail':"This activation link has already been used"},status=status)
         else:
+            content = response.json()
             return Response(content,status=status)
